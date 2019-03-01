546378f81527cd22141a1058968f8629def861e2
MrDRamos <MrDRamos@Hotmail.com>
2019-03-01 01:46:27 -0500

    Wip - Added Ground Truth labels training_GT_labels_v2.json Made input and output files independent from where the generate_submission.py is run updated the scorer code

--- test2/submit/generate_submission.py
+++ test2/submit/generate_submission.py
@@ -10,16 +10,19 @@ from generate_results import *
 import time
 import os
 
-#image_dir = 'testing/images/'
-image_dir = '../Data_Training/'
+# Run this script from the AlphaGate directory
+# python test2/submit/generate_submission.py
+script_path = os.path.dirname(os.path.realpath(__file__))
+image_dir = script_path + '/../../../Data_Training/'
+ResultFilename = script_path + "/airvision_submission.json"
 
 img_file = glob.glob(image_dir + '*.JPG')
 img_keys = [img_i.split(os.sep)[-1] for img_i in img_file]
 
-train = False
-train = True
-if train:
-    data_ofs = 0
+
+
+def UseTrainingImages(qty=20):
+    global img_keys; data_ofs = 0
     data_ofs = img_keys.index('IMG_0013.JPG')  # large + litle glare
     #data_ofs = img_keys.index('IMG_3565.JPG') # small 
     #data_ofs = img_keys.index('IMG_4443.JPG') # + ladders
@@ -28,20 +31,16 @@ if train:
     #data_ofs = img_keys.index('IMG_0638.JPG')  # large + angle
     #data_ofs = img_keys.index('IMG_0664.JPG')  # large + angle
     #data_ofs = img_keys.index('IMG_0711.JPG') # small + angle
+    img_keys = img_keys[data_ofs: data_ofs + qty]
 
-    train_qty = 20
-    Validate_qty = 2
-    img_key_train = img_keys[data_ofs : data_ofs + train_qty]
-    img_key_validate = img_keys[data_ofs + train_qty : data_ofs + train_qty + Validate_qty]
-else:
-    img_key_train = img_keys
+UseTrainingImages()
 
 # Instantiate a new detector
 finalDetector = GenerateFinalDetections()
 # load image, convert to RGB, run model and plot detections. 
 time_all = []
 pred_dict = {}
-for img_key in img_key_train:
+for img_key in img_keys:
     img =cv2.imread(image_dir + img_key)
     img =cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     tic = time.monotonic()
@@ -57,5 +56,6 @@ freq = np.round(1/mean_time,2)
 print('95% confidence interval for inference time is {0:.3f}ms +/- {1:.3f}.'.format(mean_time,ci_time))
 print('Operating frequency from loading image to getting results is {0:.2f}.'.format(freq))
 
-with open('random_submission.json', 'w') as f:
-    json.dump(pred_dict, f)
+if not (ResultFilename is None):
+    with open(ResultFilename, 'w') as f:
+        json.dump(pred_dict, f)
